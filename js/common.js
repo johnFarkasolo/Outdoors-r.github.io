151ff13010c9aeb94cbd278c41d8ea39a80887e7
Ivan Yarovyi <farkasolo@mail.ru>
2018-12-11 04:47:39 +0100

    reload :fire:

---
+++ js/common.js
@@ -0,0 +1,3 @@
+const navToggle = document.querySelector('.navigation__button');
+const navBg = document.querySelector('.navigation_background');
+const navBar = document.querySelector('.navigation__nav');
